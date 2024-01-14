aa3e1e0512595be68cf4d491b11c745c82923b0e
Musadiq Showkat Peerzada <musadiq.peerzada@gmail.com>
2024-01-14 13:52:45 +0530

    refactor(quote): keep quote length at max 200

--- components/Quote.tsx
+++ components/Quote.tsx
@@ -8,14 +8,19 @@ const QuoteComponent = () => {
   useEffect(() => {
     const fetchQuote = async () => {
       try {
-        const response = await fetch('https://raw.githubusercontent.com/skolakoda/programming-quotes-api/master/Data/quotes.json');
+        const response = await fetch(
+          'https://raw.githubusercontent.com/skolakoda/programming-quotes-api/master/Data/quotes.json',
+        );
         if (!response.ok) {
           throw new Error('Failed to fetch data');
         }
-        const data = await response.json();
-        const randomIndex = Math.floor(Math.random() * data.length);
-        setQuote(data[randomIndex].en);
-        setAuthor(data[randomIndex].author);
+        const quotes = await response.json();
+        let randomIndex = Math.floor(Math.random() * quotes.length);
+        while (quotes[randomIndex].en.length < 200) {
+          randomIndex = Math.floor(Math.random() * quotes.length);
+        }
+        setQuote(quotes[randomIndex].en);
+        setAuthor(quotes[randomIndex].author);
       } catch (error) {
         console.error('Error fetching data:', error);
       } finally {
@@ -31,9 +36,9 @@ const QuoteComponent = () => {
       {isLoading ? (
         <p>Loading...</p>
       ) : (
-        <blockquote className="quote">
-          <h1 className="dark:text-white ">{quote}</h1>
-          <p className="author">- {author}</p>
+        <blockquote className='quote'>
+          <h1 className='dark:text-white '>{quote}</h1>
+          <p className='author'>- {author}</p>
         </blockquote>
       )}
       <style jsx>{`
@@ -44,7 +49,7 @@ const QuoteComponent = () => {
           position: absolute;
           top: 90px;
           right: 20px;
-    font-family: 'Times New Roman', serif; /* Use a serif font */
+          font-family: 'Times New Roman', serif; /* Use a serif font */
           display: flex;
           flex-direction: column;
           align-items: center;
@@ -63,24 +68,23 @@ const QuoteComponent = () => {
           font-weight: 700;
         }
 
-  @media (max-width: 767px) {
-    .quote {
-      margin: 10px auto 0; /* Top margin added */
-      max-width: 80%; /* Adjust the width for center alignment */
-      right: initial;
-      top: 90px; /* Adjust the top spacing */
-    }
-  }
+        @media (max-width: 767px) {
+          .quote {
+            margin: 10px auto 0; /* Top margin added */
+            max-width: 80%; /* Adjust the width for center alignment */
+            right: initial;
+            top: 90px; /* Adjust the top spacing */
+          }
+        }
 
-  @media (min-width: 768px) {
-    .quote {
-      position: absolute;
-      top: 90px;
-      right: 20px;
-      max-width: 30%;
-    }
-  }
-
+        @media (min-width: 768px) {
+          .quote {
+            position: absolute;
+            top: 90px;
+            right: 20px;
+            max-width: 30%;
+          }
+        }
       `}</style>
     </div>
   );
