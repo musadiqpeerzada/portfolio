4f691f4e6ac5d5f3917c763c379721618b06e2e5
Musadiq Showkat Peerzada <musadiq.peerzada@gmail.com>
2024-01-30 11:11:44 +0530

    add garden

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: 'https://garden.musadiqpeerzada.com', title: 'Garden' },
   { href: '/contact', title: 'Contact' },
 ];
 
